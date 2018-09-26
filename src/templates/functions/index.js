8836d9b68451c89d94970e4f17e0f03bb8985663
Christian <christian.guest@outlook.com>
2018-09-26 08:18:48 +0100

    more bug fixes

--- src/templates/functions/index.js
+++ src/templates/functions/index.js
@@ -4,8 +4,7 @@ const libApp = require('firestudio/dist/lib/app')
 const router = require('./router')
 
 export const firestudioApp = functions.https.onRequest((request, response) => {
-  const dev = process.env.NODE_ENV !== 'production'
-  const app = libApp({ dev, conf: { distDir: 'app' } })
+  const app = libApp({ dev: false, conf: { distDir: 'app' } })
   const handler = router.getRequestHandler(app)
   console.log('File: ' + request.originalUrl) // eslint-disable-line no-console
   return app.prepare().then(() => handler(request, response))
