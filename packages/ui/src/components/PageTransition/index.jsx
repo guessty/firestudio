2b15cd8305ebb4053464d76ffa995893cd313787
Christian <christguest@hotmail.com>
2019-06-11 11:50:06 +0000

    ui: Added PageTransition

---
+++ packages/ui/src/components/PageTransition/index.jsx
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Router } from '@firestudio/core';
+//
+import Transition from '../Transition';
+
+export default class PageTransition extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    className: PropTypes.string,
+    transitionProps: PropTypes.shape({}),
+  }
+
+  static defaultProps = {
+    className: '',
+    transitionProps: {
+      enterTransition: {
+        fade: true,
+        absolute: true,
+        speed: 'normal',
+        delay: 'medium',
+      },
+      exitTransition: {
+        fade: true,
+        speed: 'normal',
+      },
+    }
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    const { router } = state;
+
+    if (router) {
+      return {
+        currentPath: router.asPath,
+      }
+    } 
+
+    return state;
+  }
+
+  state = {
+    wasTriggeredByBrowser: false,
+    router: undefined,
+    currentPath: undefined,
+  }
+
+  componentDidMount() {
+    Router.beforePopState(({ url, as, options }) => {
+      this.setState({
+        wasTriggeredByBrowser: true,
+      })
+      return true
+    });
+    this.setState({
+      router: Router.router,
+    })
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { wasTriggeredByBrowser, currentPath } = this.state;
+    if (wasTriggeredByBrowser && currentPath !== prevState.currentPath) {
+      this.setState({
+        wasTriggeredByBrowser: false,
+      })
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    const { currentPath } = this.state;
+
+    if (currentPath === nextState.currentPath && nextState.wasTriggeredByBrowser) {
+      return false;
+    }
+
+    return true;
+  }
+
+  render() {
+    const { children, className, transitionProps } = this.props;
+    const { router, wasTriggeredByBrowser } = this.state;
+
+    return router && !wasTriggeredByBrowser ? (
+      <Transition.Group className={className}>
+        <Transition
+          key={router.asPath}
+          {...transitionProps}
+          {...transitionProps.enterTransition ? {
+            enterTransition: {
+              ...transitionProps.enterTransition,
+              absolute: true,
+            }
+          } : {}}
+        >
+          <div className={className}>
+            {children}
+          </div>
+        </Transition>
+      </Transition.Group>
+    ) : (
+      <div className={className}>
+        {children}
+      </div>
+    );
+  }
+}
--- packages/ui/src/components/index.js
+++ packages/ui/src/components/index.js
@@ -4,4 +4,5 @@ export { default as Clickable } from './Clickable';
 export { default as Flex } from './Flex';
 export { default as Form, Field } from './Form';
 export { default as Transition } from './Transition';
+export { default as PageTransition } from './PageTransition';
 export { default as Loader } from './Loader';
